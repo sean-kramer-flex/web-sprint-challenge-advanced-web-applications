cbe9c541c91e67aa0678ba6164dd2a1ae3d5ac4d
Sean Kramer <seandominickramer@gmail.com>
2021-01-22 20:56:18 -0500

    Add save edit put function

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -22,6 +22,10 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
